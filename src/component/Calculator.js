d6f2b0d30d9e724ea42b2d6c2e868f0a5e9056d8
Collins Tatang <makungongcollins@gmail.com>
2021-08-10 18:06:08 +0100

    Add array button of object text

---
+++ src/component/Calculator.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const buttons = [
+  {
+    text: 'AC',
+  },
+  {
+    text: '+/-',
+  },
+  {
+    text: '%',
+  },
+  {
+    text: '÷',
+    className: 'bg-orange',
+  },
+  {
+    text: '7',
+  },
+  {
+    text: '8',
+  },
+  {
+    text: '9',
+  },
+  {
+    text: 'x',
+    className: 'bg-orange',
+  },
+  {
+    text: '4',
+  },
+  {
+    text: '5',
+  },
+  {
+    text: '6',
+  },
+  {
+    text: '-',
+    className: 'bg-orange',
+  },
+  {
+    text: '1',
+  },
+  {
+    text: '2',
+  },
+  {
+    text: '3',
+  },
+  {
+    text: '+',
+    className: 'bg-orange',
+  },
+  {
+    text: '0',
+    className: 'span-2-col',
+  },
+  {
+    text: '.',
+  },
+  {
+    text: '=',
+    className: 'bg-orange',
+  },
+];
