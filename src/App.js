50770d8a6fe202ce4d5f42f57dc576daad772361
Collins Tatang <makungongcollins@gmail.com>
2021-08-10 23:32:56 +0100

    Check linters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import React, { Component } from 'react';
 import Calculator from './component/Calculator';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return <Calculator />;
-  }
-}
+const App = () => <Calculator />;
 
 export default App;
+
