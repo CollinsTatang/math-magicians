87eb0e5d18ad4a5ed2ced0f762ffc3aaccd4d233
SimonGrchevski <simongrcevski@gmail.com>
2021-08-18 15:43:37 +0200

    Add test for the operate.js operations

---
+++ src/logic/operate.test.js
@@ -0,0 +1,46 @@
+import operate from './operate';
+
+test('-2 + 2 = 0', () => {
+  const a = -2;
+  const b = 2;
+
+  const answer = operate(a, b, '+');
+
+  expect(answer).toBe('0');
+});
+
+test('4 - 2 = 2', () => {
+  const a = 4;
+  const b = 2;
+
+  const answer = operate(a, b, '-');
+
+  expect(answer).toBe('2');
+});
+
+test('4 x 2 = 8', () => {
+  const a = 4;
+  const b = 2;
+
+  const answer = operate(a, b, 'x');
+
+  expect(answer).toBe('8');
+});
+
+test('4 ÷ 2 = 2', () => {
+  const a = 4;
+  const b = 2;
+
+  const answer = operate(a, b, '÷');
+
+  expect(answer).toBe('2');
+});
+
+test('4 % 2 = 0', () => {
+  const a = 4;
+  const b = 2;
+
+  const answer = operate(a, b, '%');
+
+  expect(answer).toBe('0');
+});
