cf6c9659a8f13a194c5f5f058c271dc92059a43f
SimonGrchevski <simongrcevski@gmail.com>
2021-08-18 21:15:20 +0200

    Add the Home component render test

---
+++ src/component/components.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import './Quote.css';
+import App from '../App';
+import Home from './Home';
+import Calculator from './Calculator'
+import Quote from './Quote';
+
+describe('Home component', () => {
+  test('Home component render',() => {
+    render(<Home />);
+    expect(screen.getByText(/Welcome to our page!/)).toBeInTheDocument;
+  })
+})
